b8286cc41f500787d78aa87a0056e812addc557c
Mariano Smorzeñuk <m_emanuel@live.com.ar>
2023-06-13 23:27:38 -0300

    Proyecto-Final

---
+++ scripts/Contact.js
@@ -0,0 +1,8 @@
+function formSend1(event) {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("mail").value;
+  alert("los campos: nombre, email y comentario, deben completarse" + name);
+  
+}
+
+
